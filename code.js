7535ad3a858297fdca3be5f3f7f6beae70848cdd
anth82bg <anth82bg@gmail.com>
2023-04-25 12:38:25 +0530

    Full website bindable ko Model

--- code.js
+++ code.js
@@ -1,12 +1,43 @@
-// Here's my data model
-var ViewModel = function (first, last) {
-    this.firstName = ko.observable(first);
-    this.lastName = ko.observable(last);
 
-    this.fullName = ko.pureComputed(function () {
-        // Knockout tracks dependencies automatically. It knows that fullName depends on firstName and lastName, because these get called when evaluating fullName.
-        return this.firstName() + " " + this.lastName();
-    }, this);
-};
+let WebsiteModel = function () {
+    const self = this;
 
-ko.applyBindings(new ViewModel("Planet", "Earth")); // This makes Knockout get to work
+    self.Pages = ko.observableArray();
+
+    self.SelectedPage = ko.observable();
+}
+
+/* ---------------------------------------------- */
+
+let WebpageModel = function (Title, LinkText, SelectedItem) {
+    const self = this;
+
+    self.isSelectedItem = ko.computed(function () {
+        return self === SelectedItem();
+    }, self);
+
+    self.Title = ko.observable(Title);
+
+    self.LinkText = ko.observable(LinkText);
+
+    self.Sidebar = ko.observableArray();
+
+    self.Sections = ko.observableArray(); /* body sections including ads */
+
+    self.Footer = ko.observable();
+}
+
+/* ---------------------------------------------- */
+
+const Model = new WebsiteModel();
+
+const HomePage = new WebpageModel('KSEB Chirakkal', 'KSEB', Model.SelectedPage);
+Model.Pages.push(HomePage);
+
+
+const AboutPage = new WebpageModel('About', 'About', Model.SelectedPage);
+Model.Pages.push(AboutPage);
+Model.SelectedPage(AboutPage);
+
+
+ko.applyBindings(Model);
