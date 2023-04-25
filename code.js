04fcbd560d5b66d0e2271cc140b28dcd1c09d4d1
anth82bg <anth82bg@gmail.com>
2023-04-25 15:04:19 +0530

    WebsiteModel not using

--- code.js
+++ code.js
@@ -29,15 +29,41 @@ let WebpageModel = function (Title, LinkText, SelectedItem) {
 
 /* ---------------------------------------------- */
 
+let ContentSection = function (metafile) {
+    const self = this;
+
+    self.metaFile = ko.observable(metafile);
+
+    self.title = ko.observable('default title');
+
+    self.list = ko.observableArray();
+
+    self.metaInfo = ko.pureComputed({
+        read: function () {
+            return self.metaFile();
+        },
+        write: function (value) {
+            $.getJSON(self.metaFile(), function (metadata) {
+                self.title(metadata.title);
+                $.getJSON(metadata.contentfile, function (data) {
+                    ko.mapping.fromJS(data, self.list);
+                });
+            });
+        }
+    });
+}
+
+/* ---------------------------------------------- */
+
 const Model = new WebsiteModel();
 
 const HomePage = new WebpageModel('KSEB Chirakkal', 'KSEB', Model.SelectedPage);
+
 Model.Pages.push(HomePage);
 
+HomePage.Sections.push(new ContentSection("./home-meta1.json"));
 
-const AboutPage = new WebpageModel('About', 'About', Model.SelectedPage);
-Model.Pages.push(AboutPage);
-Model.SelectedPage(AboutPage);
+Model.SelectedPage(HomePage);
 
 
-ko.applyBindings(Model);
+ko.applyBindings(HomePage);
